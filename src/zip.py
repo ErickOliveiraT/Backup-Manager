a239ba1a972c382cfdb8e369dab90bfebf8f4f67
Erick <erick.teixeira_50@hotmail.com>
2020-07-24 23:37:37 -0300

    zip folders

---
+++ src/zip.py
@@ -0,0 +1,16 @@
+import zipfile
+import os
+
+def zipdir(path, ziph):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+
+def build_zip(path, filename):
+    if filename.find('.zip') == -1:
+        filename += '.zip'
+    zipf = zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
+    if path[len(path)-1] != '/':
+        path += '/'
+    zipdir(path, zipf)
+    zipf.close()
