adb7c4e548153d1fbee0db2327cd23c110aeaa64
Erick <erick.teixeira_50@hotmail.com>
2020-07-30 19:11:49 -0300

    build batch windows

---
+++ src/build_batch.py
@@ -0,0 +1,29 @@
+import sys
+
+if len(sys.argv) > 1 and sys.argv[1].lower().split('-')[1] == 'windows':
+    batch = open('check.bat', 'w')
+else: sys.exit()
+
+location = input('Installation Folder: ')
+asp = chr(34)
+files = [
+    'check.py',
+    'compactor.py',
+    'credentials_drive.json',
+    'downloader.py',
+    'drive.py',
+    'filesHandler.py',
+    'interface.py',
+    'path.py',
+    'token.pickle'
+]
+
+for file in files:
+    batch.write('copy ' + asp + location + '\\' + file + asp + ' ' + asp + '.' + asp + '\n')
+batch.write('cls\n')
+batch.write('python check.py -u\n')
+batch.write('pause\n')
+for file in files:
+    batch.write('del ' + file + '\n')
+batch.write('exit')
+batch.close()
---
+++ src/test.py
@@ -0,0 +1,3 @@
+string = 'ERICK'
+
+print(string.lower())
